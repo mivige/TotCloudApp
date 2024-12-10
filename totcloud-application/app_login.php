0c7610c8ccb71578e23ec6a6da402e8f207788e7
Toni Contesti <toni.contesti.coll@gmail.com>
2024-12-10 15:47:46 +0100

    Version definitiva Toni

--- totcloud-application/app_login.php
+++ totcloud-application/app_login.php
@@ -470,6 +470,7 @@ $.ajax({
         contentType : 'application/json',
         data : form_data,
         success : function(response) {
+       
             // if response is a success, tell the user it was a successful sign up & empty the input boxes
             //$('#response').html("<div class='alert alert-success'>Successful sign up. Please login.</div>");
             //sign_up_form.find('input').val('');
